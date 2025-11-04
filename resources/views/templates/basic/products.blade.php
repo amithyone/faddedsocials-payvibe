d79135b7bbb27e989fb76a3ff7cc0e4af766d11a
amithyone <your.email@example.com>
2025-11-05 00:22:47 +0100

    Update products.blade.php

--- resources/views/templates/basic/products.blade.php
+++ resources/views/templates/basic/products.blade.php
@@ -42,7 +42,7 @@
                                 <a href="https://linktr.ee/faddedsocials"><img
                                         src="https://fadded.tomitechltd.com/assets/assets/images/slider/Banner3.jpg"
                                         alt="Banner 3"></a>
-                                <a href="https://t.me/faddedsocialsteam"><img
+                                <a href="https://chat.whatsapp.com/CuSck16moiWFH8njS7pE7C"><img
                                         src="https://fadded.tomitechltd.com/assets/assets/images/slider/Banner4.jpg"
                                         alt="Banner 4"></a>
                                 <a href="/products"><img
